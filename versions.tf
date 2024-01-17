95546fac301bb7c56f7c7f0205d5d39ef4de41ef
cypikarjun <arjun.y@cypik.com>
2024-01-17 11:03:00 +0530

    change version

--- versions.tf
+++ versions.tf
@@ -1,13 +1,11 @@
 # Terraform version
 terraform {
   required_version = ">= 1.6.6"
-}
 
-terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">=3.85.0"
+      version = ">=3.87.0"
     }
   }
 }
